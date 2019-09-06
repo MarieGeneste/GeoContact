e32f32b4b086e52382cf1d4866602869da6d9a67
JBA <jbacorrtech@gmail.com>
2019-09-06 13:29:07 +0200

    Page connexion admin

---
+++ View/Admin/index.php
@@ -0,0 +1,17 @@
+<?php $this->title = "Connexion admin"; ?>
+
+
+<div class="row d-flex flex-wrap align-items-center justify-content-center">
+    <div class="jumbotron col-s-10 d-flex flex-column align-items-center justify-content-center">
+        <h2 class="col-s-12">Connexion</h2>
+        <form class="col-s-12" action="" method="post">
+        <label>Email</label>
+        <input type="email" name="email" /><br />
+        
+        <label>Mot de passe</label>
+        <input type="password" name="password" /><br /><br />
+        
+        <input type="submit" value="Connexion" />
+        </form>
+    </div>
+</div>
