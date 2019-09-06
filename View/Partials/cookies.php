7b5dd38be61254d1be2ac458029271b0b91f62d2
Marie Geneste <marie.geneste@orange.fr>
2019-09-06 15:38:40 +0200

    Fix after merge

--- View/Partials/cookies.php
+++ View/Partials/cookies.php
@@ -1,5 +1,5 @@
-<section id="cookies">
-<p>Ce site web utilise des cookies. Vous pouvez les refuser au détriment d'une utilisation optimale. Vous pouvez modifier votre choix à tout moment dans les mentions légales du site.</p>
+<section id="cookies" class="bg-blue-light-geocontact">
+<p class="text-white font-weight-bold">Ce site web utilise des cookies. Vous pouvez les refuser au détriment d'une utilisation optimale. Vous pouvez modifier votre choix à tout moment dans les mentions légales du site.</p>
 <div id="cookiesChoices">
 <button type="button">J'accepte</button>
 <button type="button">Je refuse</button>
