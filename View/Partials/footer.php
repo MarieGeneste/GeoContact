4db3c2064a0596e13449bebcbf866bbc7d4aadb5
Marie Geneste <marie.geneste@orange.fr>
2019-09-06 12:28:35 +0200

    HTML CSS Header + footer accueil

--- View/Partials/footer.php
+++ View/Partials/footer.php
@@ -1,6 +1,9 @@
-<footer>
-  <nav class="d-flex justify-content-center">
-    <a class="nav-link" href="#">Plan du site</a>
-    <a class="nav-link" href="#">Mentions légales</a>
-  </nav>
+<footer class="container-fluid">
+    <nav class="row d-flex flex-column align-items-center justify-content-center bg-blue-light-geocontact">
+        <img src="<?= $webroot ?>/public/images/logoGeoContact2.png" width="200" alt="Logo GéoContact">
+        <div class="col s-12 d-flex flex-wrap align-items-center justify-content-around">
+            <a class="nav-link" href="#">Plan du site</a>
+            <a class="nav-link" href="#">Mentions légales</a>
+        </div>
+    </nav>
 </footer>
