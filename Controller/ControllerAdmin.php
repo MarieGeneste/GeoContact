3c82a426a9475a6fca8c54fff4bc6621a8d3acd6
Marie Geneste <marie.geneste@orange.fr>
2019-09-30 17:03:09 +0200

    Avancement authentification

--- Controller/ControllerAdmin.php
+++ Controller/ControllerAdmin.php
@@ -1,21 +1,34 @@
 <?php
 
 require_once 'Framework/Controller.php';
-// require_once 'Model/Var.php';
+require_once 'Model/User.php';
 
 class ControllerAdmin extends Controller {
-    
-    // private $var;
 
-    // public function __construct() {
-    //     $this->var = new Variable();
-    // }
+    private $User;
+
+    public function __construct() {
+        $this->User = new User();
+    }
 
     // Affiche la page de connexion
-    public function index() {
+    public function index(){
         // $vars = $this->var->getBillets();
         // $this->generateView(array('vars' => $vars));
         $this->generateView();
+    }
+
+    public function adminDashbord(){
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        $adminSession = $this->User->checkAdmin($email, $password);
+
+        if ($adminSession != null) {
+            $this->generateView(array('user' => $adminSession));
+        } else {
+            throw new Exception("Identifiants invalides", 403);
+        }
     }
 
 }
