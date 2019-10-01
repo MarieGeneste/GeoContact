8b68fdbd8f904a2092b5713d1e104838de889598
Rodrgo <contact@rodrigoesteves.fr>
2019-10-01 15:38:39 +0200

    JS terminé

--- View/Admin/adminDashboard.php
+++ View/Admin/adminDashboard.php
@@ -2,7 +2,7 @@
 <div class="container dashboard">
     <header class="head-list">
         <h2>Département</h2>
-        <span class="add-listing"><button class="add-listing-btn">
+        <span class="add-listing"><button class="add-dep-btn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                     class="feather feather-plus-circle">
@@ -65,7 +65,7 @@
     <div id="add-dep">
         <header class="head-list">
             <h2>Ajouter un département</h2>
-            <div class="add-listing"><button class="add-listing-btn">
+            <div class="add-listing"><button class="add-dep-btn">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                         class="feather feather-plus-circle">
@@ -98,7 +98,7 @@
     <div id="edit-dep">
     <header class="head-list">
         <h2>Modifier un département</h2>
-        <div id="edit-dep" class="add-listing"><button class="add-listing-btn">
+        <div id="edit-dep" class="add-listing"><button class="edit-dep-btn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                     class="feather feather-plus-circle">
@@ -138,7 +138,7 @@
 
     <header class="head-list">
         <h2>Localités</h2>
-        <span class="add-listing"><button class="add-listing-btn">
+        <span class="add-listing"><button class="add-loc-btn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                     class="feather feather-plus-circle">
@@ -201,7 +201,7 @@
     <div id="edit-loc">
         <header class="head-list">
             <h2>Editer une localité</h2>
-            <div class="add-listing"><button class="add-listing-btn">
+            <div class="add-listing"><button class="edit-loc-btn">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                         class="feather feather-plus-circle">
@@ -252,7 +252,7 @@
     <div id="add-loc">
         <header class="head-list">
             <h2>Ajouter une localité</h2>
-            <div class="add-listing"><button class="add-listing-btn">
+            <div class="add-listing"><button class="add-loc-btn">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                         class="feather feather-plus-circle">
