692d0e605b11584af79734676ea4718851413b1d
JBA <jbacorrtech@gmail.com>
2019-10-03 11:21:14 +0200

    Page plan de site + CSS

--- View/Partials/footer.php
+++ View/Partials/footer.php
@@ -1,8 +1,8 @@
 <footer class="container-fluid">
     <nav class="row d-flex flex-column align-items-center justify-content-center bg-blue-light-geocontact">
-        <img src="<?= $webroot ?>/public/images/logoGeoContact2.png" width="200" alt="Logo GéoContact">
+        <img src="<?= Configuration::get('webroot') ?>/public/images/logoGeoContact2.png" width="200" alt="Logo GéoContact">
         <div class="col s-12 d-flex flex-wrap align-items-center justify-content-around">
-            <a class="nav-link" href="#">Plan du site</a>
+            <a class="nav-link" href="<?= Configuration::get('webroot') ?>sitemap">Plan du site</a>
             <a class="nav-link" href="#">Mentions légales</a>
         </div>
     </nav>
