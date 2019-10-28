93c5913597b74263f6fdc191c34126794ef0167c
Marie Geneste <marie.geneste@orange.fr>
2019-10-28 10:10:15 +0100

    admin link footer logo

--- View/Partials/footer.php
+++ View/Partials/footer.php
@@ -1,9 +1,9 @@
 <footer class="container-fluid mt-4">
     <nav class="row d-flex flex-column align-items-center justify-content-center bg-blue-light-geocontact">
-        <img src="<?= Configuration::get('webroot') ?>/public/images/logoGeoContact2.png" width="200" alt="Logo GéoContact">
+        <a href="<?= $webroot ?>admin"><img src="<?= Configuration::get('webroot') ?>/public/images/logoGeoContact2.png" width="200" alt="Logo GéoContact"></a>
         <div class="col s-12 d-flex flex-wrap align-items-center justify-content-around">
             <a class="nav-link" href="<?= Configuration::get('webroot') ?>sitemap">Plan du site</a>
-            <a class="nav-link login-admin" href="<?= Configuration::get('webroot') ?>admin">Admin</a>
+            <!-- <a class="nav-link login-admin" href="<?= Configuration::get('webroot') ?>admin">Admin</a> -->
             <a class="nav-link" href="<?= Configuration::get('webroot') ?>legals">Mentions légales</a>
         </div>
     </nav>
