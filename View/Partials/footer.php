e84793a73171b09b30a00989adcd5ad3ea19bb69
Eric Monpeurt <ericmonpeurt@gmail.com>
2019-11-25 11:53:00 +0100

    modification footer avec affichage de la versionversion

--- View/Partials/footer.php
+++ View/Partials/footer.php
@@ -3,6 +3,8 @@
         <a href="<?= $webroot ?>admin">
             <img src="<?= Configuration::get('webroot') ?>/public/images/logoGeoContact2.png" width="200" alt="Logo GéoContact">
         </a>
+        <a href="<?= $webroot ?>admin"><img src="<?= Configuration::get('webroot') ?>/public/images/logoGeoContact2.png" width="200" alt="Logo GéoContact"></a>
+        <a>Version 1.1</a>
         <div class="col s-12 d-flex flex-wrap align-items-center justify-content-around">
             <a class="nav-link" href="<?= Configuration::get('webroot') ?>sitemap">Plan du site</a>
             <span>V1.1</span>
