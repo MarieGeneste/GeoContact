873f53d19c71322b253acc0e22990c85fe3267b8
JBA <jbacorrtech@gmail.com>
2019-11-26 13:54:53 +0100

    Merge branch 'DRO'

--- View/template.php
+++ View/template.php
@@ -4,6 +4,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="<?= Configuration::get("webroot") ?>/public/images/favicon.png">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $title ?></title>
     <link rel="stylesheet" href="<?= Configuration::get("webroot") ?>/public/css/bootstrap.min.css">
